4f8142b951b11ebd72fc2238526391adf8f0d12f
TANVIR0222 <tanvirislam3912@gmail.com>
2026-06-14 20:02:36 +0600

    chore: update dependencies in package.json for improved functionality and performance

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
+import QueryProvider from "@/components/provider/query-provider";
+import { ThemeProvider } from "@/components/provider/theme-provider";
+import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { DM_Sans, Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
+const dmSans = DM_Sans({ subsets: ["latin"], variable: "--font-sans" });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +30,26 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={cn(
+        "h-full",
+        "antialiased",
+        geistSans.variable,
+        geistMono.variable,
+        "font-sans",
+        dmSans.variable,
+      )}
+      suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <QueryProvider>
+          <body className="min-h-full flex flex-col">{children}</body>
+        </QueryProvider>
+      </ThemeProvider>
     </html>
   );
 }
